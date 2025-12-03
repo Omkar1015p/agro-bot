62ddfc2204b1bc4c86728003ef1821c2286888a8
Omkar Gawde <omkargawde662@gmail.com>
2025-12-03 10:48:45 +0530

    added the pump and seeder control buttons ON/OFF

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,36 @@
+import { useState } from "react";
+
 function App() {
   // For now we use fixed (fake) values
-  const soilMoisture = 38;   // in %
-  const pumpStatus = "OFF";  // or "ON"
+  const [pumpOn, setPumpOn] = useState(false);      // false = OFF
+  const [seederOn, setSeederOn] = useState(false);  // servo seeder
+
+  const soilMoisture = 38;           
+  const pumpStatus = pumpOn ? "ON" : "OFF";
+  const seederStatus = seederOn ? "ON" : "OFF";
 
   return (
     <div
       style={{
-        minHeight: "100vh",
+        // minHeight: "100vh",
+        height: "100vh",
+        width : "100vw",
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
-        fontFamily: "sans-serif",
-        background: "#e8f5e9",
+        fontFamily: "firacode, monospace",
+        background: "#B9FAF8",
       }}
     >
-      <h1>Agro-Bot Dashboard</h1>
+      <h1 style={{ color: "#1D1A05", fontSize: "40px", fontWeight: "700", marginBottom: "10px" }}>Agro-Bot Dashboard</h1>    
 
-      <div style={{ display: "flex", gap: "20px", marginTop: "20px" }}>
+      <div style={{ display: "flex", gap: "20px", marginTop: "15px" }}>
         {/* Soil Moisture Card */}
         <div
           style={{
             padding: "20px",
-            background: "white",
+            background: "#C7AC92",
             borderRadius: "10px",
             boxShadow: "0 2px 5px rgba(0,0,0,0.1)",
             textAlign: "center",
@@ -30,14 +38,14 @@ function App() {
           }}
         >
           <h2>Soil Moisture</h2>
-          <p style={{ fontSize: "24px", margin: 0 }}>{soilMoisture}%</p>
+          <p style={{ fontSize: "35px", margin: 0 }}>{soilMoisture}%</p>
         </div>
 
         {/* Pump Status Card */}
         <div
           style={{
             padding: "20px",
-            background: "white",
+            background: "#9381FF",
             borderRadius: "10px",
             boxShadow: "0 2px 5px rgba(0,0,0,0.1)",
             textAlign: "center",
@@ -48,7 +56,35 @@ function App() {
           <p style={{ fontSize: "24px", margin: 0, fontWeight: "bold" }}>
             {pumpStatus}
           </p>
+          <div style={{ marginTop: "12px", display: "flex", gap: "8px", justifyContent: "center" }}>
+          <button onClick={() => setPumpOn(true)}>ON</button>
+          <button onClick={() => setPumpOn(false)}>OFF</button>
+          </div>
         </div>
+
+        {/* Seeder status Card */}
+        <div
+          style={{
+            padding: "20px",
+            borderRadius: "10px",
+            background: "#4caf50",
+            boxShadow: "0 2px 5px rgba(0,0,0,0.1)",
+            width: "180px",
+            textAlign: "center",
+            color: "#fff",
+          }}
+        >
+          <h2>Seeder Status</h2>
+          <p style={{ margin: 0, fontSize: "24px", fontWeight: "bold" }}>
+            {seederStatus}
+          </p>
+
+          <div style={{ marginTop: "12px", display: "flex", gap: "8px", justifyContent: "center" }}>
+            <button onClick={() => setSeederOn(true)}>ON</button>
+            <button onClick={() => setSeederOn(false)}>OFF</button>
+          </div>
+        </div>
+
       </div>
     </div>
   );
